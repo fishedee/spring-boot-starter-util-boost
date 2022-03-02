42bac1e70a83d089136a5a7d824eafd33f5ce19e
fishedee <306766045@qq.com>
2022-03-02 15:46:12 +0800

    去掉CurdController

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ SpringBoost的Util工具库，功能就比较杂乱了，包括有：
 * JsonConfiguration，做Json的默认配置工作
 * IdGeneratorLinter，配合id-generator做code linter检查
 * ValidatorUtil，在非Bean的类上做校验工作
-* CurdController，快速生成Curd的控制器
 
 ## 安装
 
@@ -24,7 +23,7 @@ SpringBoost的Util工具库，功能就比较杂乱了，包括有：
 <dependency>
     <groupId>com.github.fishedee</groupId>
     <artifactId>spring-boot-starter-util-boost</artifactId>
-    <version>1.3</version>
+    <version>1.4</version>
 </dependency>
 ```
 
--- spring-boot-starter-util-boost-sample/src/main/java/com/fishedee/util_boost/sample/api/CategoryController.java
+++ spring-boot-starter-util-boost-sample/src/main/java/com/fishedee/util_boost/sample/api/CategoryController.java
@@ -1,26 +1,24 @@
 package com.fishedee.util_boost.sample.api;
 
-import com.fishedee.jpa_boost.CurdFilter;
-import com.fishedee.jpa_boost.CurdFilterBuilder;
-import com.fishedee.jpa_boost.CurdFilterable;
-import com.fishedee.util_boost.web.CurdController;
+import com.fishedee.jpa_boost.*;
+import com.fishedee.util_boost.annotation.TransactionalForWrite;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
-public class CategoryController extends CurdController<Category,String,CategoryDTO,CategoryController.Filter> {
+public class CategoryController{
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @PostConstruct
-    public void init(){
-        this.setCurdRepository(categoryRepository);
-    }
+    @Autowired
+    private QueryRepository queryRepository;
 
     @Data
     public static class Filter implements CurdFilterable {
@@ -42,12 +40,39 @@ public class CategoryController extends CurdController<Category,String,CategoryD
     }
 
     //GET请求 http://localhost:9191/category/search?data=%7B%22pageIndex%22%3A0%2C%22pageSize%22%3A10%7D
+    @GetMapping("search")
+    public Page<List<Category>> search(Filter filter, PageDTO dto){
+        return this.queryRepository.findByFilter(Category.class,filter,dto.getPageable().withCount().withSort("createTime desc"));
+    }
 
     //GET请求 http://localhost:9191/category/get?data=%7B%22id%22%3A%22CT2021090100000001%22%7D
+    @GetMapping("get")
+    public Category get(String id){
+        return this.categoryRepository.get(id);
+    }
 
     //POST请求 http://localhost:9191/category/add
     //{"name":"fish","remark":"a"}
+    @PostMapping("add")
+    @TransactionalForWrite
+    public void add(CategoryDTO dto){
+        Category category = new Category(dto);
+        this.categoryRepository.add(category);
+    }
 
     //POST请求 http://localhost:9191/category/del
     //{"id":"CT2021090100000001"}
+    @PostMapping("del")
+    @TransactionalForWrite
+    public void del(String id){
+        Category category = this.categoryRepository.get(id);
+        this.categoryRepository.del(category);
+    }
+
+    @PostMapping("mod")
+    @TransactionalForWrite
+    public void mod(String id,CategoryDTO dto){
+        Category category = this.categoryRepository.get(id);
+        category.mod(dto);
+    }
 }
